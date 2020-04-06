2dc5dc319075c28dedffd9cb1c392eefa15831fa
Ильнур Гиззатуллин <juvebars@mail.ru>
2020-04-06 11:13:44 +0300

    Добавьте файлы проекта.

---
+++ WPFDemo/WebsiteDataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WPFDemo
+{
+    public class WebsiteDataModel
+    {
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+}
